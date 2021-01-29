0c9b96dae3feab527ab5a7648d37975b918dcf86
maulanamania <maulanamania@gmail.com>
2021-01-29 00:09:49 -0500

    Add a landing pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'landing_pages/home'
+  get 'landing_pages/help'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
