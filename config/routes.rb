9de8f2b35588b25f2880e8253e43e2232ae87674
maulanamania <maulanamania@gmail.com>
2021-01-29 01:07:52 -0500

    Finish landing page basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'landing_pages/home'
   get 'landing_pages/help'
+  root 'landing_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
