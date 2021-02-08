e9bb9e6efa97e858f6da6ffdae8f68a0fd121af8
maulanamania <maulanamania@gmail.com>
2021-02-08 06:08:58 -0500

    Finish kanja register

--- app/controllers/kanjas_controller.rb
+++ app/controllers/kanjas_controller.rb
@@ -1,4 +1,30 @@
 class KanjasController < ApplicationController
   def new
+    @kanja = Kanja.new
   end
+  
+  def index
+  end
+  
+  def show
+    @kanja = Kanja.find(params[:id])
+  end
+  
+  def create
+    @kanja = Kanja.new(user_params)
+    if @kanja.save
+      flash[:success] = "患者を登録しました。"
+      redirect_to @kanja
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+  
+  private
+    def user_params
+      params.require(:kanja).permit(:name, :email, :barcode, :address)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'landing_pages/home'
   get 'landing_pages/help'
   root 'landing_pages#home'
+  
+  resources :kanjas
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
